9017541f95cfc169d61163d0f3f39b3b2ff4e3cd
joardershawon <68512741+joardershawon@users.noreply.github.com>
2021-12-30 23:56:21 +0600

    gg

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,160 +10,181 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
-  late AnimationController animationController;
-  late Animation<double> animation;
-  Duration duration = const Duration(milliseconds: 700);
-  int counter = 0;
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 1000),
+    vsync: this,
+  );
+
+  late Animation<double> translateX = Tween<double>(
+    begin: 10.0,
+    end: 140,
+  ).animate(
+    CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(
+        0.0,
+        0.7,
+        curve: Curves.ease,
+      ),
+    ),
+  );
+
+  late Animation<double> opacity1 = Tween<double>(
+    begin: 1,
+    end: 0,
+  ).animate(
+    CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(
+        0.0,
+        0.7,
+        curve: Curves.ease,
+      ),
+    ),
+  );
+  late Animation<double> opacity2 = Tween<double>(
+    begin: 0,
+    end: 1,
+  ).animate(
+    CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(
+        0.70,
+        1.00,
+        curve: Curves.ease,
+      ),
+    ),
+  );
+
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      duration: duration,
-      vsync: this,
-    );
-
-    final curvedAnim = CurvedAnimation(
-      parent: animationController,
-      curve: Curves.easeIn,
-      reverseCurve: Curves.easeInBack,
-    );
-
-    animation = Tween<double>(begin: 0, end: 2 * pi).animate(curvedAnim)
-      ..addListener(
-        () {
-          setState(() {});
-        },
-      );
-    // animationController.forward();
-  }
-
-  void animationReset() {
-    if (animationController.isAnimating) {
-      return;
-    } else {
-      if (animationController.isCompleted) {
-        animationController.reverse();
-      } else {
-        counter++;
-        animationController.forward();
-        Future.delayed(const Duration(seconds: 2)).then((value) {
-          animationController.reverse();
-        });
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Future.delayed(
+          const Duration(milliseconds: 1000),
+          () => _controller.reverse(),
+        );
       }
-    }
+    });
   }
 
   @override
   void dispose() {
-    print('dismissed');
-    animationController.dispose();
     super.dispose();
+    _controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final gg = animationController.value;
-    final scale = gg * (size.width * .2) * pi;
-    final boxSize = scale.clamp(80, 120.0);
-    final opacity = (gg).clamp(0.0, 1.0);
-    // print(boxSize);
-    print(gg);
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Spacer(),
-            SizedBox(
-              height: size.height / (2.2 * pi),
-              width: size.width / (.33 * pi),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Positioned(
-                    bottom: 10,
-                    child: Transform(
-                      alignment: Alignment.center,
-                      transform: Matrix4.identity()
-                        ..setEntry(3, 2, .001)
-                        ..translate(0.0, -scale / 3)
-                        ..scale(1 - gg),
-                      child: Opacity(
-                        opacity: (1 - opacity * 2).clamp(0.0, 1.0),
-                        child: const Text(
-                          'Check Your Balance',
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 10,
-                    width: scale + boxSize - 10,
-                    height: 70,
-                    child: Material(
-                      color: Colors.amber[50],
-                      elevation: 5,
-                      borderRadius: BorderRadius.circular(40),
-                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                      child: InkWell(
-                        splashColor: Colors.amber,
-                        onTap: () {
-                          animationReset();
-                        },
-                        child: Center(
-                          child: Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              Transform(
-                                alignment: Alignment.center,
-                                transform: Matrix4.identity()
-                                  ..translate((scale * (gg * .58)), 0.0)
-                                  ..rotateY(gg * 3),
-                                child: const Text(
-                                  '🪙',
-                                  style: TextStyle(
-                                    color: Colors.white70,
-                                    fontSize: 55,
-                                    fontWeight: FontWeight.w300,
-                                  ),
-                                ),
-                              ),
-                              Transform(
-                                transform: Matrix4.identity()
-                                  ..setEntry(3, 2, .001)
-                                  ..translate(0.0, 0.0),
-                                child: Opacity(
-                                  opacity: opacity < .95 ? 0.0 : opacity,
-                                  child: Text(
-                                    'Your Balance is : $counter BDT',
-                                    key: UniqueKey(),
-                                    style: const TextStyle(
-                                      color: Colors.black54,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                      shadows: [
-                                        Shadow(
-                                          color: Colors.white,
-                                          blurRadius: 10,
-                                          offset: Offset(2, 2),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+      backgroundColor: Colors.red[200],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SlidingAnimatedBox(
+                controller: _controller,
+                translateX: translateX,
+                opacity1: opacity1,
+                opacity2: opacity2,
+                height: 40,
+                width: 200,
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SlidingAnimatedBox extends StatelessWidget {
+  const SlidingAnimatedBox({
+    Key? key,
+    required AnimationController controller,
+    required this.translateX,
+    required this.opacity1,
+    required this.opacity2,
+    required this.height,
+    required this.width,
+  })  : _controller = controller,
+        super(key: key);
+
+  final AnimationController _controller;
+  final Animation<double> translateX;
+  final Animation<double> opacity1;
+  final Animation<double> opacity2;
+  final double height;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    double radius = height * .8;
+
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(50),
+      ),
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          // print(translateX.value);
+          print(radius);
+          return Stack(
+            children: [
+              Positioned(
+                top: radius * .12,
+                left: translateX.value,
+                child: GestureDetector(
+                  onTap: () {
+                    _controller.forward();
+                  },
+                  child: Container(
+                    height: radius,
+                    width: radius,
+                    decoration: BoxDecoration(
+                      color: Colors.black87,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: radius * (height / 120),
+                left: 80,
+                child: AnimatedOpacity(
+                  duration: const Duration(microseconds: 100),
+                  opacity: opacity1.value,
+                  child: const Text(
+                    "Show",
+                    // style: Styles.alertSubTitle,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: radius * (height / 120),
+                left: 80,
+                child: AnimatedOpacity(
+                  duration: const Duration(microseconds: 100),
+                  opacity: opacity2.value,
+                  child: const Text(
+                    "1Tk",
+                    // style: Styles.alertSubTitle,
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
